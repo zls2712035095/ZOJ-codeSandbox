65e54f4c2d0a2581e8d0f50433b8b774bd6befa4
zls5600 <2712035095@qq.com>
2024-05-16 13:36:03 +0800

    编写进程工具类

---
+++ src/main/java/com/zack/zojcodesandbox/utils/ProcessUtils.java
@@ -0,0 +1,69 @@
+package com.zack.zojcodesandbox.utils;
+
+import com.zack.zojcodesandbox.model.ExecuteMessage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 进程工具类
+ */
+public class ProcessUtils {
+
+    /**
+     * 执行进程并获取信息
+     *
+     * @param runProcess
+     * @param opName
+     * @return
+     */
+    public static ExecuteMessage runProcessAndGetMessage(Process runProcess, String opName) {
+        ExecuteMessage executeMessage = new ExecuteMessage();
+
+
+        try {
+            // 等待程序执行,获取错误码
+            int exitValue = runProcess.waitFor();
+            executeMessage.setExitValue(exitValue);
+            // 正常退出
+            if (exitValue == 0) {
+                System.out.println(opName + "成功");
+                // 分批获取进程的正常输出
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(runProcess.getInputStream()));
+                StringBuilder compileOutputStringBuilder = new StringBuilder();
+                // 逐行读取
+                String compileOutputLine;
+                while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                    compileOutputStringBuilder.append(compileOutputLine);
+                }
+                executeMessage.setMessage(compileOutputStringBuilder.toString());
+            } else {
+                // 异常退出
+                System.out.println(opName + "失败, 错误码：" + exitValue);
+                // 分批获取进程的正常输出
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(runProcess.getInputStream()));
+                StringBuilder compileOutputStringBuilder = new StringBuilder();
+                // 逐行读取
+                String compileOutputLine;
+                while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                    compileOutputStringBuilder.append(compileOutputLine);
+                }
+                // 分批获取进程的错误输出
+                BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(runProcess.getErrorStream()));
+                StringBuilder errorCompileOutputStringBuilder = new StringBuilder();
+                // 逐行读取
+                String errorCompileOutputLine;
+                while ((errorCompileOutputLine = errorBufferedReader.readLine()) != null) {
+                    errorCompileOutputStringBuilder.append(errorCompileOutputLine);
+                }
+                executeMessage.setErrorMessage(errorCompileOutputStringBuilder.toString());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            //throw new RuntimeException(e);
+        }
+        return executeMessage;
+    }
+
+}
